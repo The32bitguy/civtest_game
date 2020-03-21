87087251b99ac71c4a0be699c00f382856ab9ce5
R3R3R3 <>
2020-03-21 23:48:38 +0100

    Adjust dungeon loot

--- mods/dungeon_loot/loot.lua
+++ mods/dungeon_loot/loot.lua
@@ -21,14 +21,15 @@ dungeon_loot.registered_loot = {
 
 	-- minerals
 	{name = "default:coal_lump", chance = 0.9, count = {1, 12}},
-	{name = "default:gold_ingot", chance = 0.5},
-	{name = "default:steel_ingot", chance = 0.4, count = {1, 6}},
-	{name = "default:mese_crystal", chance = 0.1, count = {2, 3}},
+	{name = "default:gold_ingot", chance = 0.2},
+	{name = "default:copper_ingot", chance = 0.4, count = {1, 6}},
+	{name = "default:tin_ingot", chance = 0.4, count = {1, 6}},
+	{name = "default:bronze_ingot", chance = 0.3, count = {1, 6}},
+	{name = "default:steel_ingot", chance = 0.2, count = {1, 6}},
 
 	-- tools
 	{name = "default:sword_wood", chance = 0.6},
 	{name = "default:pick_stone", chance = 0.3},
-	{name = "default:axe_diamond", chance = 0.05},
 
 	-- natural materials
 	{name = "default:sand", chance = 0.8, count = {4, 32}, y = {-64, 32768}, types = {"normal"}},
@@ -36,7 +37,6 @@ dungeon_loot.registered_loot = {
 	{name = "default:desert_cobble", chance = 0.8, count = {4, 32}, types = {"desert"}},
 	{name = "default:dirt", chance = 0.6, count = {2, 16}, y = {-64, 32768}},
 	{name = "default:obsidian", chance = 0.25, count = {1, 3}, y = {-32768, -512}},
-	{name = "default:mese", chance = 0.15, y = {-32768, -512}},
 }
 
 function dungeon_loot.register(t)
