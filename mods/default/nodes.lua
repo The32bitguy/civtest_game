4ba4e47e5b6f9fb12f4b72743221c6e6fad0bf85
R3R3R3 <>
2020-03-31 07:06:47 +0200

    Move inadequate tool complaints out of civtest_game

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -977,42 +977,6 @@ minetest.register_node("default:aspen_leaves", {
 -- Ores
 --
 
-local has_citadella = minetest.get_modpath("citadella")
-
-minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
-   local pname = puncher:get_player_name()
-   local held = puncher:get_wielded_item()
-   local held_def = core.registered_items[held:get_name()]
-
-   if (has_citadella
-          and ct.player_modes[pname] == ct.PLAYER_MODE_REINFORCE)
-      or held:get_name():find("sword")
-   then
-      return
-   end
-
-   local node_def = core.registered_nodes[node.name]
-   local node_level = (node_def
-                          and node_def.groups
-                          and node_def.groups.level) or 0
-
-   local toolcaps = held_def.tool_capabilities
-   local tool_level = (toolcaps and toolcaps.max_drop_level) or 0
-
-   if tool_level < node_level then
-      local held_desc = held_def.description
-      if tool_level == 0 then
-         held_desc = "your hand"
-      end
-
-      local node_desc = node_def.description
-      minetest.chat_send_player(
-         pname, "You will not get a drop from "
-            .. node_desc .. " if you dig with " .. held_desc .. "."
-      )
-   end
-end)
-
 minetest.register_node("default:stone_with_coal", {
 	description = "Coal Ore",
 	tiles = {"default_stone.png^default_mineral_coal.png"},
